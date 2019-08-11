a6a2e66b8b049145a3afb8c4a1653d05a0573d0e
andornaut <andornaut@users.noreply.github.com>
2019-08-10 20:36:27 -0400

    Update StartDate param in PurgeAfterFourWeeks filter

--- init.sql
+++ init.sql
@@ -22,7 +22,7 @@ UPDATE Config Set Value = 0 WHERE Name = "ZM_WEB_RESIZE_CONSOLE";
 
 UPDATE Controls SET MaxPanRange = 45, MaxPanStep = 45, MaxTiltRange = 10, HasTiltSpeed = 1, MaxTiltStep = 10, MaxTiltSpeed = 4, HasPresets = 0 WHERE Name = "Foscam 9831W";
 
-INSERT INTO Filters VALUES (NULL, 'PurgeAfterFourWeeks','{\"terms\":[{\"attr\":\"Archived\",\"op\":\"=\",\"val\":\"0\"},{\"cnj\":\"and\",\"attr\":\"Date\",\"op\":\"<\",\"val\":\"-4 week\"}],\"sort_field\":\"DateTime\",\"sort_asc\":\"1\",\"limit\":\"10000\"}',0,0,0,0,0,0,NULL,1,0,0,0,1,0);
+INSERT INTO Filters VALUES (NULL, 'PurgeAfterFourWeeks','{\"terms\":[{\"attr\":\"Archived\",\"op\":\"=\",\"val\":\"0\"},{\"cnj\":\"and\",\"attr\":\"StartDate\",\"op\":\"<\",\"val\":\"-4 week\"}],\"sort_field\":\"DateTime\",\"sort_asc\":\"1\",\"limit\":\"10000\"}',0,0,0,0,0,0,NULL,1,0,0,0,1,0);
 
 INSERT INTO MonitorPresets VALUES (NULL,'Amcrest IP4M-1025E FFMPEG H.264','Ffmpeg','/dev/video<?>','0',255,'rtsp','rtpRtsp','<username>:<pwd>@<ip-address>','443','rtsp://<username>:<pwd>@<ip-address>:554/cam/realmonitor?channel=1&subtype=0','',1280,720,0,0.00,0,'0','','',100,100);
 INSERT INTO MonitorPresets VALUES (NULL,'Foscam FI9803PV2 FFMPEG H.264','Ffmpeg','/dev/video<?>','0',255,'rtsp','rtpRtsp','<username>:<pwd>@<ip-address>','443','rtsp://<username>:<pwd>@<ip-address>:443/videoMain','',1280,720,0,0.00,0,'0','','',100,100);
